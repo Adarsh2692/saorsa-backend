1aa922d16e85372d751e5211641ded28be974476
Adarsh2692 <adarsh.singh2692@gmail.com>
2021-05-24 11:17:33 +0530

    Route added to push mcq score

--- routes/api/courses.js
+++ routes/api/courses.js
@@ -1,65 +1,39 @@
-const express = require("express");
-const request = require("request");
-const config = require("config");
+const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
-const auth = require("../../middleware/auth");
-const adminAuth = require("../../middleware/adminAuth");
-const Course = require("../../models/Course");
-const multerFunc = require("../../middleware/multer");
+const auth = require('../../middleware/auth');
+const adminAuth = require('../../middleware/adminAuth');
+const Course = require('../../models/Course');
+const multerFunc = require('../../middleware/multer');
 const multerUploads = multerFunc.multerUploads;
 const dataUri = multerFunc.dataUri;
-const cloudinary = require("../../config/cloudinaryConfig");
+const cloudinary = require('../../config/cloudinaryConfig');
 const uploader = cloudinary.uploader;
 const cloudinaryConfig = cloudinary.cloudinaryConfig;
-
-//@route   POST api/courses/new
-//@desc    Test route
-//@access  Private
-router.post("/new",multerUploads, async (req, res) => {
-	const { courseName, musicLink, exerciseLink } = req.body;
-
-	let courseImage = "";
-
-	//Here we accept a file and upload it to clouinary, get a link for that and save it to db
-	if (req.file) {
-		const file = dataUri(req).content;
-		await uploader.upload(file).then((result) => {
-			courseImage = result.url;
-		});
-	}
-
-	//build newCourse object
-	const newCourse = {
-		courseName,
-		courseImage,
-		musicLink,
-		exerciseLink,
-	};
-
-	try {
-		//add this course to database
-		const latestCourse = new Course(newCourse);
-
-		await latestCourse.save();
-
-		res.json(latestCourse);
-	} catch (err) {
-		console.error(error.message);
-		res.status(500).send("Server Error");
-	}
-});
 
 //@route   GET api/courses/all
 //@desc    Test route
 //@access  Private
-router.get("/all", async (req, res) => {
-    try {
-        const courses = await Course.find();
-        res.json(courses);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
-    }
+router.get('/all', async (req, res) => {
+	try {
+		const courses = await Course.find();
+		res.json(courses);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+router.get('/one', async (req, res) => {
+	const { name } = req.body;
+	try {
+		const course = await Course.find({name});
+		res.json(course);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 module.exports = router;
--- routes/api/mcq.js
+++ routes/api/mcq.js
@@ -3,14 +3,32 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Mcq = require('../../models/Mcq');
 
-router.get('/', auth, async(req,res)=>{
-    try {
-        let mcqArray= await Mcq.findOne({user:req.user.id});
-        res.json(mcqArray);
-    } catch (err) {
-        console.error(err.message);
+//@route   GET api/mcq
+//@desc    Get mcq score array
+//@access  Private
+router.get('/', auth, async (req, res) => {
+	try {
+		let mcqArray = await Mcq.findOne({ user: req.user.id });
+		res.json(mcqArray);
+	} catch (err) {
+		console.error(err.message);
 		res.status(500).send('Server Error');
-    }
-})
+	}
+});
+
+//@route   PUT api/mcq
+//@desc    Push score to mcq array
+//@access  Private
+router.put('/', auth, async (req, res) => {
+	const { mcq, score } = req.body;
+	try {
+		let mcqArray = await Mcq.findOne({ user: req.user.id });
+		mcqArray.sums.forEach((e) => {
+			e.sumArray.push(score);
+		});
+		mcqArray.save();
+		res.json(mcqArray);
+	} catch (err) {}
+});
 
 module.exports = router;
